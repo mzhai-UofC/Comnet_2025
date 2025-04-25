a0e416d7dccd498963e62bdf349625fdd5e17573
mzhai <mzhai@ucalgary.ca>
2025-04-25 13:51:10 -0700

    Init cart signal in client side & init home page contents

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './features/home/home.component';
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
+import { CartComponent } from './features/cart/cart.component';
  
  export const routes: Routes = [
      {path: '', component: HomeComponent},
      {path: 'shop', component: ShopComponent},
      {path: 'shop/:id', component: ProductDetailsComponent},
+     {path: 'cart', component: CartComponent},
      {path: 'test-error', component: TestErrorComponent},
      {path: 'not-found', component: NotFoundComponent},
      {path: 'server-error', component: ServerErrorComponent},
