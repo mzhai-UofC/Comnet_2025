0755b45aecf42e2439208c4b2221eb87ad2f3d52
mzhai <mzhai@ucalgary.ca>
2025-05-01 16:10:15 -0700

    Completed Indentity on client's side

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -3,9 +3,12 @@ import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {MatProgressBar} from '@angular/material/progress-bar';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { CartService } from '../../core/services/cart.service';
+import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
@@ -17,6 +20,9 @@ import { CartService } from '../../core/services/cart.service';
     RouterLink,
     RouterLinkActive,
     MatProgressBar,
+    MatMenu,
+    MatDivider,
+    MatMenuTrigger
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -24,4 +30,17 @@ import { CartService } from '../../core/services/cart.service';
 export class HeaderComponent {
   busyService = inject(BusyService);
   cartService = inject(CartService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout(){
+    this.accountService.logout().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      },
+      error: (error) => console.log(error)
+      
+    })
+  }
 }
