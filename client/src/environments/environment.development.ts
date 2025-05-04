7f4a3d063943c6977ecc1a234ed56b226390df04
mzhai <mzhai@ucalgary.ca>
2025-05-03 22:23:11 -0700

    Set up Stripe and create payment intent components

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51RKmmIQ0ysIxrutYTrswlr6gUEJj1Blp0YEWesuaVb7Koxykt392hB69OB7PaGTwh1seBhtcfRagEFL3ysul0nih00PcQ50fF2'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51RKmmIQ0ysIxrutYTrswlr6gUEJj1Blp0YEWesuaVb7Koxykt392hB69OB7PaGTwh1seBhtcfRagEFL3ysul0nih00PcQ50fF2',
 };
