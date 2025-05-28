7962c5dba02d8aa878e01684fae61edb8d560ed8
mzhai <mzhai@ucalgary.ca>
2025-05-27 17:53:20 -0700

    Add production environment

--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -42,7 +42,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+    .WithOrigins("https://fatbuniverse.azurewebsites.net","http://localhost:4200","https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
