f0db8256b98f61562294130ae3ee46117d653c1f
mzhai <mzhai@ucalgary.ca>
2025-05-27 19:25:39 -0700

    Update production environment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/',
-    hubUrl: 'https://localhost:5001/hub/notifications',
+    // apiUrl: 'api/',
+    // hubUrl: 'https://localhost:5001/hub/notifications',
+    apiUrl: 'https://fatbuniverse.azurewebsites.net/api/',
+    hubUrl: 'https://fatbuniverse.azurewebsites.net/hub/notifications',
     stripePublicKey: 'pk_test_51RTQWRPJDZ50TngPuWbRWh21fXx4tlw83SGfyhrivnGhjTBPO7s1LH4yeQhsco9phVr31arbhpmbxw46sv7bPUp400WYIG9Yb9',
 };
